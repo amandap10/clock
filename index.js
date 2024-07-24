9968e3425dd0902105d3afb603a98bc03a503815
Amanda Oliveira <amanda_passos@outlook.com>
2024-07-24 04:35:58 -0300

    treining basic concepts

---
+++ index.js
@@ -0,0 +1,19 @@
+function update(){
+    let display = document.querySelector('.display');
+
+    let now = new Date();
+    let hours = correctTime(now.getHours()) + ":" + correctTime(now.getMinutes()) + ":" + correctTime(now.getSeconds());
+
+
+display.textContent = hours;
+}
+
+function correctTime(numbers){
+    if(numbers < 10){
+        numbers = '0' + numbers;
+    }
+    return numbers;
+}
+
+
+setInterval(update, 1000);
